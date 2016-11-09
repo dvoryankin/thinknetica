6d1e1efbfa5ee1775388dcbf47748074c2ab5ab8
pchellovod <pchellovod@gmail.com>
2016-11-09 21:51:07 +0500

    rename everything

--- test.rb
+++
@@ -1,9 +0,0 @@
-alphabet = ("a".."z").to_a
-
-hash = Hash[alphabet.map.with_index.to_a]
-
-hash.each {|key, value| hash[key] = value + 1 }
-
-hash.delete_if {|key, value| key =~ /\A[^aeiou]/}
-
-puts hash
